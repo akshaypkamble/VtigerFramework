a37fcbb33d6272b002f0493c5009169012391b1e
hp <hp@LAPTOP-SAR5P7K4>
2022-09-23 00:27:21 +0530

    fixing

---
+++ src/test/java/MavenTest/test0005.java
@@ -0,0 +1,16 @@
+package MavenTest;
+
+import org.testng.annotations.Test;
+
+public class test0005 {
+	
+	@Test
+	public void contact()
+	{
+		System.out.println("This is my first commit - MavenProjects");
+		
+		System.out.println("ok");
+	}
+
+
+}
